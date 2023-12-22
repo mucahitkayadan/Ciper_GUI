2a375cfae8c4bf2f7399a5726a5e9e9a097c2ca7
mucahitkayadan <muhammetkayadan@hotmail.com>
2023-12-22 06:17:44 -0600

    Algorithm screen updated

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from PyQt5.QtWidgets import QApplication, QStackedWidget
 from welcome_screen import WelcomeScreen
 import sys
+from PyQt5.QtGui import QIcon
 
 
 class Main:
     def __init__(self):
         self.app = QApplication(sys.argv)
+        self.icon = QIcon("ui/img/icon.png")
         self.widget = QStackedWidget()
         self.welcome = WelcomeScreen(self.widget)
         self.widget.addWidget(self.welcome)
         self.widget.setFixedHeight(800)
         self.widget.setFixedWidth(1200)
+        self.widget.setWindowIcon(self.icon)
         self.widget.show()
 
     def run(self):
--- select_algorithm_window.py
+++ select_algorithm_window.py
@@ -1,8 +1,32 @@
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
+import sqlite3
 
 
 class AlgorithmWindow(QMainWindow):
     def __init__(self):
         super(AlgorithmWindow, self).__init__()
         loadUi("ui/select_algorithm_window.ui", self)
+        self.radio_buttons = [
+            self.caesar_radio_button,
+            self.substitution_radio_button,
+            self.vigenere_radio_button,
+            self.rail_fence_radio_button,
+            self.rot13_radio_button
+        ]
+        for radio_button in self.radio_buttons:
+            radio_button.toggled.connect(self.select_algorithm)
+
+    def select_algorithm(self):
+        conn = sqlite3.connect("db/credentials.db")
+        cur = conn.cursor()
+        query = "SELECT caesar, substitution, vigenere, rail_fence, rot13 FROM crypto_descriptions"
+        cur.execute(query)
+        results = cur.fetchone()
+
+        for i, radio_button in enumerate(self.radio_buttons):
+            if radio_button.isChecked():
+                self.algo_title.setText(results[i])
+                break
+
+        conn.close()
