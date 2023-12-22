9e0932f7ffaf4615ca305f5f99b00711b6a2764d
mucahitkayadan <muhammetkayadan@hotmail.com>
2023-12-22 12:18:52 -0600

    Updated

--- select_algorithm_window.py
+++ select_algorithm_window.py
@@ -1,12 +1,13 @@
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
-import sqlite3
+from PyQt5.QtCore import QUrl
 
 
 class AlgorithmWindow(QMainWindow):
     def __init__(self):
         super(AlgorithmWindow, self).__init__()
         loadUi("ui/select_algorithm_window.ui", self)
+        self.text_browser.setSource(QUrl.fromLocalFile("descriptions/caesar.html"))
         self.radio_buttons = [
             self.caesar_radio_button,
             self.substitution_radio_button,
@@ -14,19 +15,16 @@ class AlgorithmWindow(QMainWindow):
             self.rail_fence_radio_button,
             self.rot13_radio_button
         ]
+        self.html_elements = [
+            'caesar', 'substitution', 'vigenere', 'rail_fence', 'rot13'
+        ]
         for radio_button in self.radio_buttons:
             radio_button.toggled.connect(self.select_algorithm)
 
     def select_algorithm(self):
-        conn = sqlite3.connect("db/credentials.db")
-        cur = conn.cursor()
-        query = "SELECT caesar, substitution, vigenere, rail_fence, rot13 FROM crypto_descriptions"
-        cur.execute(query)
-        results = cur.fetchone()
-
         for i, radio_button in enumerate(self.radio_buttons):
             if radio_button.isChecked():
-                self.algo_title.setText(results[i])
+                cipher_name = self.html_elements[i]
+                file_path = "descriptions/" + cipher_name + ".html"
+                self.text_browser.setSource(QUrl.fromLocalFile(file_path))
                 break
-
-        conn.close()
