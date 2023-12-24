8793b1892a21aa3e29ae53d31f3639e6ff44e20c
mucahitkayadan <muhammetkayadan@hotmail.com>
2023-12-24 17:10:45 -0600

    New ui file added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QStackedWidget
+from PyQt5.QtWidgets import QApplication, QStackedWidget, QDesktopWidget
 from welcome_screen import WelcomeScreen
 import sys
 from PyQt5.QtGui import QIcon
@@ -7,14 +7,28 @@ from PyQt5.QtGui import QIcon
 class Main:
     def __init__(self):
         self.app = QApplication(sys.argv)
+        self.screen = QDesktopWidget().screenGeometry()
         self.icon = QIcon("ui/img/icon.png")
         self.widget = QStackedWidget()
         self.welcome = WelcomeScreen(self.widget)
         self.widget.addWidget(self.welcome)
+
+        self.widget.setMinimumSize(1200, 800)
+        # self.widget.setMaximumSize(self.screen.width(), self.screen.height())
+        # self.welcome.setGeometry(0, 0, self.screen.width(), self.screen.height())
+        # self.widget.setGeometry(0, 0, self.screen.width(), self.screen.height())
+
         self.widget.setFixedHeight(800)
         self.widget.setFixedWidth(1200)
         self.widget.setWindowIcon(self.icon)
         self.widget.show()
+        # self.center_widget()
+
+    def center_widget(self):
+        widget_geometry = self.widget.frameGeometry()
+        screen_center = QDesktopWidget().availableGeometry().center()
+        widget_geometry.moveCenter(screen_center)
+        self.widget.move(widget_geometry.topLeft())
 
     def run(self):
         try:
