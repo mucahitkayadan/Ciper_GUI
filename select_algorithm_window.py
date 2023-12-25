efa5a40749a2520b1e08a1e162aa466ac6ab332a
mucahitkayadan <muhammetkayadan@hotmail.com>
2023-12-25 01:10:58 -0600

    Algorithms added

--- select_algorithm_window.py
+++ select_algorithm_window.py
@@ -7,7 +7,9 @@ class AlgorithmWindow(QMainWindow):
     def __init__(self):
         super(AlgorithmWindow, self).__init__()
         loadUi("ui/select_algorithm_window.ui", self)
-        self.text_browser.setSource(QUrl.fromLocalFile("descriptions/caesar.html"))
+        self.description_text_browser.setSource(QUrl.fromLocalFile("html_files/descriptions/caesar.html"))
+        self.algorithm_text_browser.setSource(QUrl.fromLocalFile("html_files/algorithms/caesar.html"))
+        self.tabWidget.setCurrentIndex(0)
         self.radio_buttons = [
             self.caesar_radio_button,
             self.substitution_radio_button,
@@ -25,6 +27,8 @@ class AlgorithmWindow(QMainWindow):
         for i, radio_button in enumerate(self.radio_buttons):
             if radio_button.isChecked():
                 cipher_name = self.html_elements[i]
-                file_path = "descriptions/" + cipher_name + ".html"
-                self.text_browser.setSource(QUrl.fromLocalFile(file_path))
+                description_file_path = "html_files/descriptions/" + cipher_name + ".html"
+                algorithm_file_path = "html_files/algorithms/" + cipher_name + ".html"
+                self.description_text_browser.setSource(QUrl.fromLocalFile(description_file_path))
+                self.algorithm_text_browser.setSource(QUrl.fromLocalFile(algorithm_file_path))
                 break
