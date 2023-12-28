b62cdf1264bc9ddd1d15b607701b8cb7c288e408
mucahitkayadan <muhammetkayadan@hotmail.com>
2023-12-28 07:02:25 -0600

    Algorithms added

---
+++ engine/Cipher.py
@@ -0,0 +1,141 @@
+import codecs
+
+
+class Cipher:
+    def __init__(self):
+        self.encryption_functions = [
+            self.caesar_cipher,
+            self.vigenere_cipher,
+            self.rail_fence_cipher,
+            self.rot13_cipher,
+            self.substitution_cipher
+        ]
+
+    def encrypt(self, text, index, *args):
+        if index < 0 or index >= len(self.encryption_functions):
+            raise ValueError("Invalid index")
+
+        encryption_function = self.encryption_functions[index]
+        return encryption_function(text, *args)
+
+    @staticmethod
+    def caesar_cipher(text, shift, encrypt=True):
+        result = ''
+        for char in text:
+            if char.isalpha():
+                offset = ord('A') if char.isupper() else ord('a')
+                result += chr((ord(char) - offset + shift * (1 if encrypt else -1)) % 26 + offset)
+            else:
+                result += char
+        return result
+
+    @staticmethod
+    def vigenere_cipher(text, key, encrypt=True):
+        result = ''
+        key_index = 0
+        for char in text:
+            if char.isalpha():
+                offset = ord('A') if char.isupper() else ord('a')
+                key_char = key[key_index % len(key)]
+                key_offset = ord('A') if key_char.isupper() else ord('a')
+                result += chr((ord(char) - offset +
+                               (ord(key_char) - key_offset) * (1 if encrypt else -1)) % 26 + offset)
+                key_index += 1
+            else:
+                result += char
+        return result
+
+    @staticmethod
+    def rail_fence_cipher(text, rails, encrypt=True):
+        if encrypt:
+            return ''.join(text[i::rails] for i in range(rails))
+        else:
+            cycle_length = 2 * (rails - 1)
+            result = [' '] * len(text)
+            index = 0
+
+            for i in range(rails):
+                j = 0
+                while j + i < len(text):
+                    result[j + i] = text[index]
+                    index += 1
+                    if i != 0 and i != rails - 1 and j + cycle_length - i < len(text):
+                        result[j + cycle_length - i] = text[index]
+                        index += 1
+                    j += cycle_length
+
+            return ''.join(result)
+
+    @staticmethod
+    def rot13_cipher(text):
+        return codecs.encode(text, 'rot_13')
+
+    @staticmethod
+    def substitution_cipher(text, substitution_key, encrypt=True):
+        if encrypt:
+            return ''.join(substitution_key.get(char, char) for char in text)
+        else:
+            reverse_key = {v: k for k, v in substitution_key.items()}
+            return ''.join(reverse_key.get(char, char) for char in text)
+
+    # Decryption methods
+
+    def caesar_decipher(self, text, shift):
+        return self.caesar_cipher(text, shift, encrypt=False)
+
+    def vigenere_decipher(self, text, key):
+        return self.vigenere_cipher(text, key, encrypt=False)
+
+    def rot13_decipher(self, text):
+        return self.rot13_cipher(text)
+
+    def substitution_decipher(self, text, substitution_key):
+        return self.substitution_cipher(text, substitution_key, encrypt=False)
+
+#
+# # Example Usage:
+# cipher_instance = Cipher()
+# # Define the substitution key
+# substitution_key_ = {'A': 'Q', 'B': 'W', 'C': 'E', 'D': 'R',
+#                       'E': 'T', 'F': 'Y', 'G': 'U', 'H': 'I', 'I': 'O', 'J': 'P',
+#                      'K': 'A', 'L': 'S', 'M': 'D', 'N': 'F', 'O': 'G', 'P': 'H',
+#                      'Q': 'J', 'R': 'K', 'S': 'L', 'T': 'Z',
+#                      'U': 'X', 'V': 'C', 'W': 'V', 'X': 'B', 'Y': 'N', 'Z': 'M'}
+#
+#
+# # Caesar Cipher
+# encrypted_caesar = cipher_instance.caesar_cipher("HELLO", shift=3, encrypt=True)
+# decrypted_caesar = cipher_instance.caesar_cipher(encrypted_caesar, shift=3, encrypt=False)
+#
+# # Vigenère Cipher
+# encrypted_vigenere = cipher_instance.vigenere_cipher("HELLO", key="KEY", encrypt=True)
+# decrypted_vigenere = cipher_instance.vigenere_cipher(encrypted_vigenere, key="KEY", encrypt=False)
+#
+# # Rail Fence Cipher
+# encrypted_rail_fence = cipher_instance.rail_fence_cipher("HELLO", rails=3, encrypt=True)
+# decrypted_rail_fence = cipher_instance.rail_fence_cipher(encrypted_rail_fence, rails=3, encrypt=False)
+#
+# # ROT13 Cipher
+# encrypted_rot13 = cipher_instance.rot13_cipher("HELLO")
+# decrypted_rot13 = cipher_instance.rot13_cipher(encrypted_rot13)
+#
+# # Substitution Cipher
+# encrypted_substitution = cipher_instance.substitution_cipher("HELLO", substitution_key_, encrypt=True)
+# decrypted_substitution = cipher_instance.substitution_cipher(encrypted_substitution, substitution_key_, encrypt=False)
+#
+# # Print Results
+# print("Caesar Cipher:")
+# print("Encrypted:", encrypted_caesar)
+# print("Decrypted:", decrypted_caesar)
+# print("\nVigenère Cipher:")
+# print("Encrypted:", encrypted_vigenere)
+# print("Decrypted:", decrypted_vigenere)
+# print("\nRail Fence Cipher:")
+# print("Encrypted:", encrypted_rail_fence)
+# print("Decrypted:", decrypted_rail_fence)
+# print("\nROT13 Cipher:")
+# print("Encrypted:", encrypted_rot13)
+# print("Decrypted:", decrypted_rot13)
+# print("\nSubstitution Cipher:")
+# print("Encrypted:", encrypted_substitution)
+# print("Decrypted:", decrypted_substitution)
--- select_algorithm_window.py
+++ select_algorithm_window.py
@@ -1,6 +1,9 @@
+from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import QUrl
+
+from engine.Cipher import Cipher
 
 
 class AlgorithmWindow(QMainWindow):
@@ -10,6 +13,9 @@ class AlgorithmWindow(QMainWindow):
         self.description_text_browser.setSource(QUrl.fromLocalFile("html_files/descriptions/caesar.html"))
         self.algorithm_text_browser.setSource(QUrl.fromLocalFile("html_files/algorithms/caesar.html"))
         self.tabWidget.setCurrentIndex(0)
+        self.cipher = Cipher
+        self.input_text = None
+        self.key_text = None
         self.radio_buttons = [
             self.caesar_radio_button,
             self.substitution_radio_button,
@@ -21,14 +27,40 @@ class AlgorithmWindow(QMainWindow):
             'caesar', 'substitution', 'vigenere', 'rail_fence', 'rot13'
         ]
         for radio_button in self.radio_buttons:
-            radio_button.toggled.connect(self.select_algorithm)
+            radio_button.toggled.connect(self.update_tabs)
+        self.submit_button.clicked.connect(self.update_application)
 
-    def select_algorithm(self):
+    def update_tabs(self):
+        self.update_description()
+        self.update_algorithm()
+
+    def update_description(self):
         for i, radio_button in enumerate(self.radio_buttons):
             if radio_button.isChecked():
                 cipher_name = self.html_elements[i]
                 description_file_path = "html_files/descriptions/" + cipher_name + ".html"
-                algorithm_file_path = "html_files/algorithms/" + cipher_name + ".html"
                 self.description_text_browser.setSource(QUrl.fromLocalFile(description_file_path))
+                break
+
+    def update_algorithm(self):
+        for i, radio_button in enumerate(self.radio_buttons):
+            if radio_button.isChecked():
+                cipher_name = self.html_elements[i]
+                algorithm_file_path = "html_files/algorithms/" + cipher_name + ".html"
                 self.algorithm_text_browser.setSource(QUrl.fromLocalFile(algorithm_file_path))
                 break
+
+    def update_application(self):
+        self.input_text = self.application_text_input.toPlainText()
+        self.key_text = self.key_input.toPlainText()
+        if not self.input_text or not self.key_text:
+            self.application_text_output.setTextColor(QColor.fromRgb(255, 0, 0))
+            self.application_text_output.setPlainText("Please fill out text and key!")
+            self.application_text_output.setTextColor(QColor.fromRgb(0, 0, 0))
+            return
+        print(self.input_text)
+        print(self.key_text)
+
+        if self.caesar_radio_button.isChecked():
+            encrypted_text = self.cipher.caesar_cipher(self.input_text, int(self.key_text), encrypt=True)
+            self.application_text_output.setPlainText(encrypted_text)
